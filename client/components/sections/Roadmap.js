d943a77e51477dc70cbbcf0998ccb5034c4855f3
Francesco Sullo <sullof@gmail.com>
2022-05-24 18:11:06 -0700

    Fix roadmap

--- client/components/sections/Roadmap.js
+++ client/components/sections/Roadmap.js
@@ -1,14 +1,5 @@
-// eslint-disable-next-line no-undef
-import CenteredImage from './CenteredImage';
-
-const {Container, Row, Col} = ReactBootstrap
-import _ from 'lodash'
 import * as Scroll from 'react-scroll'
-import Markdown from 'react-markdown-it'
-
-
 import Base from '../Base'
-import Ab from '../Ab'
 
 const roadmap = {
   '2018': ['Invention of Everdragons, the first cross-chain non-fungible token (NFT) project'],
@@ -26,19 +17,21 @@ const roadmap = {
   ],
   'Q1/Q2 2022': [
     'Giveaways of 350 Everdragons2 Genesis Governance tokens to the community',
-    'Public sale of 650 Everdragons2 Genesis Governance tokens',
-    'Building DeFi for staking/lending infrastructure',
-    'Decentralized autonomous organization (DAO) governance setup and first Community Treasury vote',
-    'In-house development for Origins game',
-    'Public sale of 9001 Everdragons2 Genesis tokens',
-    'Strategic partnerships to increase the redeemable value of each NFT for digital and physical goods and services'
+    'Public sale of 250 Everdragons2 Genesis Governance tokens',
+    'Reveal and airdrop of the 600 Genesis Governance tokens',
+    'Release of the Everdragons2 MiniGame'
   ],
   'Q3/Q4 2022': [
+    'Building Everdragons2 GameFi protocol, for staking/lending infrastructure',
+    'Decentralized autonomous organization (DAO) governance setup and first Community Treasury vote',
+    'Public sale of 9401 Everdragons2 Genesis tokens'
+  ],
+  '2023': [
     'Partnerships with popular games to integrate Everdragons2 as a cross-game asset',
+    'In-house development for Origins game',
     'Launch of Origins play-to-earn crypto-metaverse game'
   ]
 }
-
 
 export default class Roadmap extends Base {
 
@@ -53,36 +46,21 @@ export default class Roadmap extends Base {
   roadmapList() {
     const rows = []
     for (let step in roadmap) {
-      rows.push(<h4 className={'roadmapStep'} key={'key'+Math.random()}>{step}</h4>)
+      rows.push(<h4 className={'roadmapStep'} key={'key' + Math.random()}>{step}</h4>)
       for (let elem of roadmap[step]) {
-        rows.push(<li className={'roadmapElem'}  key={'key'+Math.random()}>{elem}</li>)
+        rows.push(<li className={'roadmapElem'} key={'key' + Math.random()}>{elem}</li>)
       }
     }
     return rows
   }
 
-  // <div className={'home-section'}>
-  //   <Scroll.Element name='roadmap'><h1>Roadmap</h1></Scroll.Element>
-  //   <div key={'key' + Math.random()} className={'textBlock roadmap'}>
-  //     {this.roadmapList()}
-  //   </div>
-  // </div>
-
-  //   <Row>
-  //     <Col>
-  //       <div className={'textBlock'} style={{ padding: 0 }}>
-  //         <img src="/images/roadmap.png" width="100%" />
-  //       </div>
-  //     </Col>
-  // </Row>
-
   render() {
     return (
       <div className={'home-section'}>
         <Scroll.Element name='roadmap'><h1>Roadmap</h1></Scroll.Element>
-          <div key={'key' + Math.random()} className={'textBlock roadmap'}>
-            {this.roadmapList()}
-          </div>
+        <div key={'key' + Math.random()} className={'textBlock roadmap'}>
+          {this.roadmapList()}
+        </div>
       </div>
     )
   }
